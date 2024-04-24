80de0379f354043d017ff5e8d4917350d130d193
B. Ricks <bricks@unomaha.edu>
2024-04-24 11:08:40 -0500

    Simplify code

--- games/maps-example/components/CameraMoverComponent.js
+++ games/maps-example/components/CameraMoverComponent.js
@@ -7,17 +7,10 @@ class CameraMoverComponent extends Component {
   update(ctx) {
 
     let scene = Engine.currentScene;
-    let cameraZoom = Camera.main.transform.scaleX
-
+    
+    
     let logicalWidthNoZoom = scene.logicalWidth
     let logicalHeightNoZoom = logicalWidthNoZoom/scene.aspectRatio
-
-    let logicalWidthZoomed = logicalWidthNoZoom / cameraZoom;
-    let logicalHeightZoomed = logicalHeightNoZoom / cameraZoom;
-
-    let cameraX = Camera.main.transform.x;
-    let cameraY = Camera.main.transform.y;
-
 
 
     let screenXWOLetterBoxing = Input.mousePosition.x
@@ -28,13 +21,6 @@ class CameraMoverComponent extends Component {
 
     let cameraXAsPercent = screenXWithLetterBoxing/scene.logicalWidthViewWidthInPixels;
     let cameraYAsPercent = screenYWithLetterBoxing/scene.logicalWidthViewHeightInPixels;
-
-    let worldXWOZoom = cameraX + cameraXAsPercent*logicalWidthNoZoom;
-    let worldYWOZoom = cameraY + cameraYAsPercent*logicalHeightNoZoom;
-
-    let worldXWithZoom = cameraX + cameraXAsPercent*logicalWidthZoomed;
-    let worldYWithZoom = cameraY + cameraYAsPercent*logicalHeightZoomed;
-
 
 
     //Do the actual movement
@@ -59,7 +45,11 @@ class CameraMoverComponent extends Component {
       //Now for the zooming
       if (Input.wheelDelta != 0) {
         console.log(Input.wheelDelta)
+
+        //Store to calculate offset
         let cameraZoom1 = cameraZoom
+
+        //Soom based on the sign of wheel delta
         if (Input.wheelDelta > 0) {
 
           cameraZoom *= 1.1;
@@ -73,8 +63,10 @@ class CameraMoverComponent extends Component {
           Camera.main.transform.scaleY = cameraZoom
         }
 
+        //Now keep the mouse in the same place
         let cameraDeltaX = cameraXAsPercent*logicalWidthNoZoom*(1/cameraZoom1-1/cameraZoom)
         let cameraDeltaY = cameraYAsPercent*logicalHeightNoZoom*(1/cameraZoom1-1/cameraZoom)
+        
         Camera.main.transform.x += cameraDeltaX;
         Camera.main.transform.y += cameraDeltaY;
       }
@@ -88,7 +80,9 @@ class CameraMoverComponent extends Component {
     ctx.font = "50px Sans"
 
     let scene = Engine.currentScene;
-    let cameraZoom = Camera.main.transform.scaleX
+    let camera = Camera.main;
+
+    let cameraZoom = camera.transform.scaleX
 
     let logicalWidthNoZoom = scene.logicalWidth
     let logicalHeightNoZoom = logicalWidthNoZoom/scene.aspectRatio
@@ -96,8 +90,8 @@ class CameraMoverComponent extends Component {
     let logicalWidthZoomed = logicalWidthNoZoom / cameraZoom;
     let logicalHeightZoomed = logicalHeightNoZoom / cameraZoom;
 
-    let cameraX = Camera.main.transform.x;
-    let cameraY = Camera.main.transform.y;
+    let cameraX = camera.transform.x;
+    let cameraY = camera.transform.y;
 
 
 
@@ -120,13 +114,30 @@ class CameraMoverComponent extends Component {
 
     let textX = 100;
     let textY = 100;
-    let margin = 55;
+    let margin = 65;
+
+    ctx.fillText("Logical Size No Zoom: " + logicalWidthNoZoom + " " + logicalHeightNoZoom, textX, textY)
+    textY += margin
+
+    ctx.fillText("Logical Size Zoomed: " + logicalWidthZoomed.toFixed(2) + " " + logicalHeightZoomed.toFixed(2), textX, textY)
+    textY += margin
+    
+
+    ctx.fillText("Camera.main.scale: " + cameraZoom.toFixed(2), textX, textY)
+    textY += margin;
+
+    ctx.fillText("Camera.main.transform: " + cameraX.toFixed(2) + " " + cameraY.toFixed(2), textX, textY)
+    textY += margin;
+
+    textY += margin/2
 
     ctx.fillText("Input.mousePosition (Screen): " + screenXWOLetterBoxing + " " + screenYWOLetterBoxing, textX, textY)
     textY += margin
+    
+    ctx.fillText("this.lastMouse: " + this.lastMouseX + " " + this.lastMouseY, textX, textY)
+    textY += margin;
 
-
-    ctx.fillText("Input.mousePosition (Screen LB): " + screenXWithLetterBoxing + " " + screenYWithLetterBoxing, textX, textY)
+    ctx.fillText("Input.mousePosition (Screen LetterBoxing): " + screenXWithLetterBoxing + " " + screenYWithLetterBoxing, textX, textY)
     textY += margin
 
     ctx.fillText("Camera %: " + cameraXAsPercent.toFixed(2) + " " + cameraYAsPercent.toFixed(2), textX, textY)
@@ -138,14 +149,8 @@ class CameraMoverComponent extends Component {
     ctx.fillText("World (with zoom): " + worldXWithZoom.toFixed(2) + " " + worldYWithZoom.toFixed(2), textX, textY)
     textY += margin
 
-    ctx.fillText("this.lastMouse: " + this.lastMouseX + " " + this.lastMouseY, textX, textY)
-    textY += margin;
     
-    ctx.fillText("Camera.main.scale: " + cameraZoom.toFixed(2), textX, textY)
-    textY += margin;
-
-    ctx.fillText("Camera.main.transform: " + cameraX.toFixed(2) + " " + cameraY.toFixed(2), textX, textY)
-    textY += margin;
+    
 
 
   }
